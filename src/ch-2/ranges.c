b9045ea5171d043723e2dcc8563d3b3ab7faf8f5
Raoul Millais <raoul@raoulmillais.com>
2014-04-15 21:17:13 +0100

    add fp types to ranges

--- src/ch-2/ranges.c
+++ src/ch-2/ranges.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include <math.h>
+#include <float.h>
 
 #define BYTE 8
 
-void print_type_info (const char *name, long max, ssize_t sz)
+void print_integer_type_info (const char *name, long max, ssize_t sz)
 {
 	printf ("%s is %ld bytes\n", name, sz);
 	printf ("%s from %ld to %ld\n", name, max / 2, -(max / 2));
@@ -19,10 +20,13 @@ int main (void)
 	int_max = pow (2, sizeof(int) * BYTE);
 	long_max = pow (2, sizeof(long) * BYTE);
 
-	print_type_info ("char", (long)char_max, sizeof(char));
-	print_type_info ("short", (long)short_max, sizeof(short));
-	print_type_info ("int", (long)int_max, sizeof(int));
-	print_type_info ("long", (long)long_max, sizeof(long));
+	print_integer_type_info ("char", (long)char_max, sizeof(char));
+	print_integer_type_info ("short", (long)short_max, sizeof(short));
+	print_integer_type_info ("int", (long)int_max, sizeof(int));
+	print_integer_type_info ("long", (long)long_max, sizeof(long));
+
+	printf ("float is from %f to %f\n", FLT_MIN, FLT_MAX);
+	printf ("double is from %lf to %lf\n", DBL_MIN, DBL_MAX);
 
 
 	return 0;
